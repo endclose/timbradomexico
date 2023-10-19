d28113d4dab545dad9b8f8890366d044ead16654
Benjamin Bailon <benjaminbailon@MacBook-Air.local>
2023-10-18 22:33:38 -0600

    JUST TEST

--- lib/timbradomexico.lib.php
+++ lib/timbradomexico.lib.php
@@ -1,76 +1,55 @@
 <?php
-/* Copyright (C) 2023 Benjamin Bailón Salas <b.bailon@stgmining.com>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
+require_once DOL_DOCUMENT_ROOT.'/custom/timbradomexico/vendor/autoload.php';
 
-/**
- * \file    timbradomexico/lib/timbradomexico.lib.php
- * \ingroup timbradomexico
- * \brief   Library files with common functions for TimbradoMexico
- */
-
-/**
- * Prepare admin pages header
- *
- * @return array
- */
-function timbradomexicoAdminPrepareHead()
+use Facturapi\Facturapi;
+class FacturaHandle
 {
-	global $langs, $conf;
+	private $facturapi;
 
-	// global $db;
-	// $extrafields = new ExtraFields($db);
-	// $extrafields->fetch_name_optionals_label('myobject');
-
-	$langs->load("timbradomexico@timbradomexico");
-
-	$h = 0;
-	$head = array();
-
-	$head[$h][0] = dol_buildpath("/timbradomexico/admin/setup.php", 1);
-	$head[$h][1] = $langs->trans("Settings");
-	$head[$h][2] = 'settings';
-	$h++;
-
-	/*
-	$head[$h][0] = dol_buildpath("/timbradomexico/admin/myobject_extrafields.php", 1);
-	$head[$h][1] = $langs->trans("ExtraFields");
-	$nbExtrafields = is_countable($extrafields->attributes['myobject']['label']) ? count($extrafields->attributes['myobject']['label']) : 0;
-	if ($nbExtrafields > 0) {
-		$head[$h][1] .= ' <span class="badge">' . $nbExtrafields . '</span>';
+	public function __construct($token)
+	{
+		$this->facturapi = new Facturapi($token);
 	}
-	$head[$h][2] = 'myobject_extrafields';
-	$h++;
-	*/
 
-	$head[$h][0] = dol_buildpath("/timbradomexico/admin/about.php", 1);
-	$head[$h][1] = $langs->trans("About");
-	$head[$h][2] = 'about';
-	$h++;
+	public function formatInvoice($object){
+		$object->fetch_thirdparty();
 
-	// Show more tabs from modules
-	// Entries must be declared in modules descriptor with line
-	//$this->tabs = array(
-	//	'entity:+tabname:Title:@timbradomexico:/timbradomexico/mypage.php?id=__ID__'
-	//); // to add new tab
-	//$this->tabs = array(
-	//	'entity:-tabname:Title:@timbradomexico:/timbradomexico/mypage.php?id=__ID__'
-	//); // to remove a tab
-	complete_head_from_modules($conf, $langs, null, $head, $h, 'timbradomexico@timbradomexico');
+		$invoice = new stdClass();
+		$invoice->customer = new stdClass();
+		$invoice->customer->address = new stdClass();
+		$invoice->items = array();
 
-	complete_head_from_modules($conf, $langs, null, $head, $h, 'timbradomexico@timbradomexico', 'remove');
+		$invoice->customer->legal_name = $object->thirdparty->name;
+		$invoice->customer->tax_id = $object->thirdparty->idprof1;
+		$invoice->customer->tax_system = strval(intval(explode('-',$object->thirdparty->forme_juridique)[0]));
 
-	return $head;
+		$invoice->customer->address->zip = $object->thirdparty->zip;
+
+		foreach($object->lines as $line){
+			$item_line = new stdClass();
+			$item_line->quantity = $line->qty;
+			$item_line->product = new stdClass();
+			$item_line->product->description = $line->libelle;
+			$item_line->product->product_key = '60131324';
+			$item_line->product->price = $line->subprice;
+			$item_line->product->tax_included = false;
+
+			$invoice->items[] = $item_line;
+		}
+
+		$invoice->payment_form = '28';
+		$invoice->payment_method = 'PUE';
+		$invoice->currency = 'USD';
+
+
+		return $invoice;
+	}
+
+	public function createInvoice($object){
+		$invoice = $this->formatInvoice($object);
+		var_dump($invoice);
+		$res = $this->facturapi->Invoices->create($invoice);
+
+		return $res;
+	}
 }
